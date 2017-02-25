81e5a8b29f5caeadfc9aaed16e5f1c130eeedc16
MelanieBerrut <melanieberrut.dev@gmail.com>
2017-02-25 23:49:39 +0000

    [style] Added styles to image component

--- src/app/image-component/image.component.ts
+++ src/app/image-component/image.component.ts
@@ -2,7 +2,20 @@ import { Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-image',
-	template: '<img src="{{imageUrl}}" />',
+	template: '<div class="thumb"><img src="{{imageUrl}}" /></div>',
+	styles: [
+		`
+			.thumb {
+				width: 100px;
+				height: 100px;
+				padding: 10px;
+			}
+			.thumb img {
+				height: 100%;
+				width: auto;
+			}
+		`
+	]
 })
 
 export class ImageComponent {
